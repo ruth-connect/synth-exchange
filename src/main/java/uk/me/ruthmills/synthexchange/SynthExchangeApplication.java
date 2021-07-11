db37c933eb4cc1ef517c815a319d895d751e27ee
ruth <ruth@DESKTOP-HOVL7PT>
2021-07-11 14:38:59 +0100

    Add initial Spring Boot stuff.

---
+++ src/main/java/uk/me/ruthmills/synthexchange/SynthExchangeApplication.java
@@ -0,0 +1,13 @@
+package uk.me.ruthmills.synthexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynthExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynthExchangeApplication.class, args);
+	}
+
+}
