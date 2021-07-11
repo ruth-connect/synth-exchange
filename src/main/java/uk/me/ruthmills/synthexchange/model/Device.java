f5a77ba7fca6cac404c0378ad83e489deacd4297
ruth <ruth@DESKTOP-HOVL7PT>
2021-07-11 20:07:34 +0100

    Try using annotations to determine subclass.

--- src/main/java/uk/me/ruthmills/synthexchange/model/Device.java
+++ src/main/java/uk/me/ruthmills/synthexchange/model/Device.java
@@ -1,5 +1,11 @@
 package uk.me.ruthmills.synthexchange.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({ @Type(value = MidiDevice.class, name = "midi") })
 public class Device {
 
 	private String manufacturer;
