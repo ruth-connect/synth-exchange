0b6c828c224de44e1e4b94c03f553717719372d2
ruth <ruth@DESKTOP-HOVL7PT>
2021-07-11 16:01:29 +0100

    Add a PostConstruct for the MainView.

--- src/main/java/uk/me/ruthmills/synthexchange/view/MainView.java
+++ src/main/java/uk/me/ruthmills/synthexchange/view/MainView.java
@@ -2,8 +2,9 @@ package uk.me.ruthmills.synthexchange.view;
 
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
@@ -12,7 +13,6 @@ import com.vaadin.flow.router.Route;
 import uk.me.ruthmills.synthexchange.service.MidiService;
 
 @Route
-@Component
 public class MainView extends VerticalLayout {
 
 	private static final long serialVersionUID = 1L;
@@ -20,17 +20,23 @@ public class MainView extends VerticalLayout {
 	@Autowired
 	private MidiService midiService;
 
-	public MainView() {
-		Set<String> midiDevices = midiService.getMidiDevices();
+	private Select<String> midiInputSelect;
+	private Select<String> midiOutputSelect;
 
-		Select<String> midiInputSelect = new Select<>();
+	public MainView() {
+		midiInputSelect = new Select<>();
 		midiInputSelect.setLabel("MIDI Input");
-		midiInputSelect.setItems(midiDevices);
 		add(midiInputSelect);
 
-		Select<String> midiOutputSelect = new Select<>();
+		midiOutputSelect = new Select<>();
 		midiOutputSelect.setLabel("MIDI Output");
-		midiOutputSelect.setItems(midiDevices);
 		add(midiOutputSelect);
+	}
+
+	@PostConstruct
+	public void initialise() {
+		Set<String> midiDevices = midiService.getMidiDevices();
+		midiInputSelect.setItems(midiDevices);
+		midiOutputSelect.setItems(midiDevices);
 	}
 }
