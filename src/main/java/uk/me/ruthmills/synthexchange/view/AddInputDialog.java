8faad1b368d416821272b0c2168461260c037911
ruth <ruth@DESKTOP-HOVL7PT>
2021-07-12 19:46:05 +0100

    Get MIDI inputs to show.

--- src/main/java/uk/me/ruthmills/synthexchange/view/AddInputDialog.java
+++ src/main/java/uk/me/ruthmills/synthexchange/view/AddInputDialog.java
@@ -22,44 +22,52 @@ import uk.me.ruthmills.synthexchange.service.MidiService;
 public class AddInputDialog extends Dialog {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private DeviceMappingService deviceMappingService;
-	
+
 	private FormLayout formLayout;
 	private Select<Device> deviceSelect;
+	private Select<javax.sound.midi.MidiDevice.Info> midiInterfaceSelect;
 	private Button addInputButton;
 	private Button cancelButton;
-	
+
 	@Autowired
-	public AddInputDialog(MidiService midiService, DeviceService deviceService, DeviceMappingService deviceMappingService) {
-		
+	public AddInputDialog(MidiService midiService, DeviceService deviceService,
+			DeviceMappingService deviceMappingService) {
+
 		this.deviceMappingService = deviceMappingService;
-		
+
 		formLayout = new FormLayout();
 		formLayout.add(new Text("Add Input"));
 		add(formLayout);
-		
+
 		deviceSelect = new Select<>();
 		deviceSelect.setLabel("Device");
 		deviceSelect.setItemLabelGenerator(Device::getName);
 		deviceSelect.setItems(deviceService.getDevices());
 		formLayout.add(deviceSelect);
-		
+
+		midiInterfaceSelect = new Select<>();
+		midiInterfaceSelect.setLabel("MIDI Interface");
+		midiInterfaceSelect.setItemLabelGenerator(javax.sound.midi.MidiDevice.Info::getName);
+		midiInterfaceSelect.setItems(midiService.getMidiInputs());
+		formLayout.add(midiInterfaceSelect);
+
 		addInputButton = new Button("Add Input", e -> addInput());
 		formLayout.add(addInputButton);
-		
+
 		cancelButton = new Button("Cancel", e -> cancel());
 		formLayout.add(cancelButton);
 	}
-	
+
 	private void addInput() {
 		Device device = deviceSelect.getValue();
 		if (device != null) {
 			DeviceMapping deviceMapping = new DeviceMapping();
 			deviceMapping.setManufacturer(device.getManufacturer());
-			
+
 			if (device instanceof MidiDevice) {
-				MidiDevice midiDevice = (MidiDevice)device;
+				MidiDevice midiDevice = (MidiDevice) device;
 				deviceMapping.setModel(midiDevice.getModel());
 			}
 
@@ -68,7 +76,7 @@ public class AddInputDialog extends Dialog {
 		deviceSelect.setValue(null);
 		this.close();
 	}
-	
+
 	private void cancel() {
 		deviceSelect.setValue(null);
 		this.close();
