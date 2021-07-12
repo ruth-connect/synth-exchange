1ace09ef125dd9b24af59dab3e82e5e5cb99d6c4
ruth <ruth@DESKTOP-HOVL7PT>
2021-07-12 18:48:33 +0100

    Use data providers.

--- src/main/java/uk/me/ruthmills/synthexchange/view/MainView.java
+++ src/main/java/uk/me/ruthmills/synthexchange/view/MainView.java
@@ -9,6 +9,8 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Route;
 
 import uk.me.ruthmills.synthexchange.model.mapping.DeviceMapping;
@@ -19,6 +21,9 @@ import uk.me.ruthmills.synthexchange.service.MidiService;
 public class MainView extends VerticalLayout {
 
 	private static final long serialVersionUID = 1L;
+
+	private ListDataProvider<DeviceMapping> inputDataProvider;
+	private ListDataProvider<DeviceMapping> outputDataProvider;
 
 	private Grid<DeviceMapping> inputs;
 	private Grid<DeviceMapping> outputs;
@@ -31,9 +36,10 @@ public class MainView extends VerticalLayout {
 		Label inputsLabel = new Label("Inputs");
 		add(inputsLabel);
 
+		inputDataProvider = DataProvider.ofCollection(deviceMappingService.getInputs());
 		inputs = new Grid<>(DeviceMapping.class);
 		inputs.setColumns("manufacturer", "model", "connection", "channel");
-		inputs.setItems(deviceMappingService.getInputs());
+		inputs.setDataProvider(inputDataProvider);
 		inputs.setHeightByRows(true);
 		add(inputs);
 
@@ -44,9 +50,10 @@ public class MainView extends VerticalLayout {
 		Label outputsLabel = new Label("Outputs");
 		add(outputsLabel);
 
+		outputDataProvider = DataProvider.ofCollection(deviceMappingService.getOutputs());
 		outputs = new Grid<>(DeviceMapping.class);
 		outputs.setColumns("manufacturer", "model", "connection", "channel");
-		outputs.setItems(deviceMappingService.getOutputs());
+		outputs.setDataProvider(outputDataProvider);
 		outputs.setHeightByRows(true);
 		add(outputs);
 
