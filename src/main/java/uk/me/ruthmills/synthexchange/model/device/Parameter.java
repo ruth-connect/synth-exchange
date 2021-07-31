bc49ee3157f721144d67ffba20e262f9bb202253
ruth <ruth@DESKTOP-HOVL7PT>
2021-07-31 18:00:03 +0100

    Create an abstract Parameter class.

---
+++ src/main/java/uk/me/ruthmills/synthexchange/model/device/Parameter.java
@@ -0,0 +1,14 @@
+package uk.me.ruthmills.synthexchange.model.device;
+
+public abstract class Parameter {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
